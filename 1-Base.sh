dc69ba4f129676bea7cf9664c9a877cd571860fd
kory Freese <koryfreese@gmail.com>
2017-10-21 22:52:44 -0700

    Minor Changes

--- 1-Base.sh
+++ 1-Base.sh
@@ -6,6 +6,9 @@ sudo pacman -S xorg-xrandr vlc feh xorg-xinit xarchiver nemo chromium xfce4-term
 echo Installing Codecs
 sudo pacman -S a52dec x264 x265 faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer gst-plugins-ugly gst-plugins-base gst-plugins-bad gst-libav
 
+echo Installing Video Drivers
+sudo pacman -S xf86-video-vesa xf86-video-ati mesa mesa-libgl lib32-mesa lib32-mesa-libgl
+
 echo Installing AUR Packages
 yaourt -S albert compiz emerald xarchiver
 
